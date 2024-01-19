eda4ce554884e60a11bc489bf27c9ff1399953b8
Nihad <nihadamirov98@gmail.com>
2024-01-19 23:10:27 +0400

    Add controller

---
+++ src/main/java/com/company/quizapp/controller/QuestionController.java
@@ -0,0 +1,16 @@
+package com.company.quizapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestions")
+    public String getAllQuestion(){
+        return "";
+
+    }
+}
