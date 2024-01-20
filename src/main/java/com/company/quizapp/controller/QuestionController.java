db81d7c58bd4b79002828a587d6866c2f2ccb6d4
Nihad <nihadamirov98@gmail.com>
2024-01-20 21:12:03 +0400

    Add sevice

--- src/main/java/com/company/quizapp/controller/QuestionController.java
+++ src/main/java/com/company/quizapp/controller/QuestionController.java
@@ -1,16 +1,24 @@
 package com.company.quizapp.controller;
 
+import com.company.quizapp.model.Question;
+import com.company.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
 
+    @Autowired
+    QuestionService questionService;
+
     @GetMapping("allQuestions")
-    public String getAllQuestion(){
-        return "";
+    public List<Question> getAllQuestion(){
+        return questionService.getAllQuestions;
 
     }
 }
---
+++ src/main/java/com/company/quizapp/service/QuestionService.java
@@ -0,0 +1,11 @@
+package com.company.quizapp.service;
+
+import com.company.quizapp.model.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    public List<Question> getAllQuestions;
+}
